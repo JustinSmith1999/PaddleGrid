deeee8d06a543ddb0e4f74638b1468157635cfcd
JustinSmith1999 <152663439+JustinSmith1999@users.noreply.github.com>
2025-12-13 23:38:11 -0500

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App.tsx';
 import './index.css';
 import { ThemeProvider } from './contexts/ThemeContext';
@@ -23,7 +22,6 @@ createRoot(document.getElementById('root')!).render(
       <ThemeProvider>
         <App />
       </ThemeProvider>
-      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   </StrictMode>
 );
